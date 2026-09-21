7e16c3e192db9fe030aa60405d3c946d9066888f
rollygao <rolly.gao@mail.utoronto.ca>
2026-09-21 14:02:24 -0400

    Complete Task 1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,5 +1,7 @@
 public class DataTypes {
     public static long sum(int[] numbers) {
+        //the original code did not pass because the test case uses a number that is larger than 32bits integer
+        //so it causes an overflow and fails
         long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
